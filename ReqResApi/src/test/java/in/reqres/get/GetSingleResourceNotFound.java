4fa5bf82aad47ff33ee446b7e983e72305d450bc
YamahaNmax <m4mitzlearning@gmail.com>
2023-12-27 10:26:11 +0000

    Push

---
+++ ReqResApi/src/test/java/in/reqres/get/GetSingleResourceNotFound.java
@@ -0,0 +1,29 @@
+package in.reqres.get;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GetSingleResourceNotFound {
+
+    RequestSpecification requestSpecification;
+    Response response;
+    ValidatableResponse validatableResponse;
+
+    @Test
+    public void getSingleResourceNotFoundBdd() {
+        RestAssured.baseURI = "https://reqres.in/api/unknown/23";
+        requestSpecification = RestAssured.given().log().all();
+        response = requestSpecification.get();
+        response.then().log().all();
+        String statusLine = response.getStatusLine();
+        Assert.assertEquals(statusLine, "HTTP/1.1 404 Not Found");
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 404);
+
+
+    }
+}
