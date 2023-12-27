11d9c8c05ba5099bf77cd39371980b9b6a2dd9b7
YamahaNmax <m4mitzlearning@gmail.com>
2023-12-27 09:14:09 +0000

    Push

---
+++ ReqResApi/src/test/java/in/reqres/homework/GetListUsers.java
@@ -0,0 +1,64 @@
+package in.reqres.homework;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class GetListUsers {
+
+    RequestSpecification requestSpecification;
+    Response response;
+    ValidatableResponse validatableResponse;
+
+    @Test
+    public void getListUsersWay1() {
+        given().log().all()
+                .when()
+                .get("https://reqres.in/api/users?page=2")
+                .then().log().all()
+                .statusCode(200);
+    }
+
+    @Test
+    public void getListUsersWay2() {
+        RestAssured.baseURI = "https://reqres.in/api/users?page=2";
+        requestSpecification = RestAssured.given();
+        response = requestSpecification.get();
+        System.out.println(response.prettyPrint());
+        validatableResponse = response.then();
+        validatableResponse.statusCode(200);
+
+    }
+
+    @Test
+    public void getListUsersWayBdd() {
+        RestAssured.baseURI = "https://reqres.in/api/users?page=2";
+        requestSpecification = RestAssured.given().log().all();
+        response = requestSpecification.get();
+        response.then().log().all(); //for log purpose you need to use then
+        //System.out.println(response.prettyPrint());
+        String statusLine = response.getStatusLine();
+        Assert.assertEquals(statusLine, "HTTP/1.1 200 OK");
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 200);
+    }
+
+    @Test
+    public void verifyUserDetails() {
+        given().log().all()
+                .when()
+                .get("https://reqres.in/api/users?page=2")
+                .then().log().all()
+                .statusCode(200)
+                .body("data[2].id", equalTo(9))
+                .body("data[2].first_name", equalTo("Tobias"))
+                .body("data[2].last_name", equalTo("Funke"));
+    }
+
+}
---
+++ ReqResApi/src/test/java/in/reqres/homework/GetSingleUser.java
@@ -0,0 +1,44 @@
+package in.reqres.homework;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class GetSingleUser {
+
+    RequestSpecification requestSpecification;
+    Response response;
+    ValidatableResponse validatableResponse;
+
+    @Test
+    public void getSingleUserBdd() {
+        RestAssured.baseURI = "https://reqres.in/api/users/12";
+        requestSpecification = given().log().all();
+        response = requestSpecification.get();
+        System.out.println(response.prettyPrint());
+        String statusLine = response.getStatusLine();
+        Assert.assertEquals(statusLine, "HTTP/1.1 200 OK");
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 200);
+    }
+
+    @Test
+    public void verifyUserDetails() {
+        given().log().all()
+                .when()
+                .get("https://reqres.in/api/users/12")
+                .then().log().all()
+                .statusCode(200)
+                .body("data.first_name", equalTo("Rachel"))
+                .body("support.text",equalTo("To keep ReqRes free, contributions towards server costs are appreciated!"));
+
+
+    }
+
+}
